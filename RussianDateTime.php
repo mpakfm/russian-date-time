41cd185c9c5dbe832d7b7c801920dd06bce5712f
mpakfm <nm4505028256>
2019-08-12 03:04:41 +0300

    Final version 1.0.0

--- RussianDateTime.php
+++ RussianDateTime.php
@@ -9,22 +9,50 @@
 
 namespace Mpakfm;
 
+use mysql_xdevapi\Exception;
+
 class RussianDateTime {
 
     const FORMAT_TYPE_LC_DEFAULT = 0; // пятница 3 февраля
     const FORMAT_TYPE_UC_FIRST   = 1; // Пятница 3 Февраля
     const FORMAT_TYPE_UC_WORDS   = 2; // Пятница 3 февраля
     const FORMAT_TYPE_UC_ALL     = 3; // ПЯТНИЦА 3 ФЕВРАЛЯ
-    
+
     const FORMAT_NAME = 0;
     const FORMAT_BY   = 1;
     const FORMAT_INTO = 2;
-    
+
+    const DEFAULT_DELIMITER = ', ';
+
     private static $caseVariant = [
         self::FORMAT_NAME, self::FORMAT_BY, self::FORMAT_INTO
     ];
 
-        private static $dayName = [
+    private static $phpDaysFormat = [
+        'd', 'D', 'j', 'l', 'N', 'S', 'w', 'z', 'W'
+    ];
+
+    private static $phpMonthFormat = [
+        'F', 'm', 'M', 'n', 't',
+    ];
+
+    private static $phpYearFormat = [
+        'L', 'o', 'Y', 'y',
+    ];
+
+    private static $phpTimeFormat = [
+        'a', 'A', 'B', 'g', 'G', 'h', 'H', 'i', 's', 'u', 'v',
+    ];
+
+    private static $phpTimezoneFormat = [
+        'e', 'I', 'O', 'P', 'T', 'Z',
+    ];
+
+    private static $phpFullDateTimeFormat = [
+        'c', 'r', 'U',
+    ];
+
+    private static $dayName = [
         1 => 'понедельник',
         2 => 'вторник',
         3 => 'среда',
@@ -101,12 +129,14 @@ class RussianDateTime {
     
     public static $case = 0;
 
+    public static $listDelimiter = ', ';
+
     /**
      * D и l	Текстовое представление дня месяца, например Пн либо Среда      От Пн до Вс либо от Понедельник до Воскресение
      * F и M	Текстовое представление месяца, например Января или Сен         С Января по Декабря либо с Янв по Дек
      * f	Текстовое представление месяца, например января	                С января по декабря
      */
-    public static function format(string $format, \DateTime $dateTime, int $case = 0) {
+    public static function format(string $format, \DateTime $dateTime, int $case = 0): string {
         if (in_array($case, static::$caseVariant)) {
             static::$case = $case;
         }
@@ -133,50 +163,229 @@ class RussianDateTime {
         return $dateTime->format($format);
     }
 
-    public static function listFormat(string $format, \DateTime $begin, \DateTime $end) {
+    /**
+     * Строковое представление периода из дней через запятую
+     * Важно учитывать что период задается от и до поэтому время считается и выводится по времени установленному в объекте DateTime $begin
+     * @todo продумать вывод времени с ЧЧ:ММ по ЧЧ:ММ где время используется от обоих объектов DateTime: $begin, $end
+     *  Падеж по умолчанию: Понедельник, но Января
+     *      "j f", $d1, $d2           => 2, 3, 4 мая
+     *      "l d F", $d1, $d2         => Понедельник 09, Вторник 10, Среда 11 Ноября
+     *      "j F Yг. H:i", $d1, $d2   => 30, 31 Дек 2019г., 1 Янв 2020г 20:00.
+     *  Падежы передаются в параметрах:
+     *      "в l j f H:i", $d1, $d2, RussianDateTime::FORMAT_INTO                                      => в среду 9, четверг 10 февраля 12:00
+     *      "l d H:i F Yг.", $d1, $d2, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME, ';' => 30 Чт; 31 Пт Февраль 20:00; 01 Сб Март 20:00 2019г.
+     *      "F j H:i Yг.", $d1, $d2, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME, ';'   => Февраль 30; 31 20:00; Март 1 20:00 2019г.
+     *
+     */
+    public static function listFormat(string $format, \DateTime $begin, \DateTime $end, int $caseDay = 0, int $caseMonth = 1, string $delimiter = null): string {
+        if (!in_array($caseDay, static::$caseVariant)) {
+            $caseDay = static::FORMAT_NAME;
+        }
+        if (!in_array($caseMonth, static::$caseVariant)) {
+            $caseMonth = static::FORMAT_BY;
+        }
+        if (!is_null($delimiter)) {
+            static::$listDelimiter = $delimiter;
+        } else {
+            static::$listDelimiter = static::DEFAULT_DELIMITER;
+        }
+
+        $onceTimeFormat = array_merge(static::$phpTimeFormat, static::$phpTimezoneFormat);
+        $allFormat      = array_merge(static::$phpDaysFormat, static::$phpMonthFormat, static::$phpYearFormat, $onceTimeFormat);
+
+        $onceTimePosition = [];
+        $yearPosition     = [];
+        $monthPosition    = [];
+        $dayPosition      = [];
+
+        $onceTimeFormatString = '';
+        $yearFormatString     = '';
+        $monthFormatString    = '';
+        $dayFormatString      = '';
+        $unknownFormatString  = '';
+        $typeFormatLetters    = null;
+
+        for ($i = 0; $i < strlen($format); $i++) {
+            $letter = $format[$i];
+            if (in_array($letter, static::$phpFullDateTimeFormat)) {
+                throw new \Exception('Cannot use full format characters for date list: ' . implode(', ', static::$phpFullDateTimeFormat));
+            }
+            if (in_array($letter, $onceTimeFormat)) {
+                $typeFormatLetters    = 'ONCE';
+                $onceTimePosition[]   = strpos($format, $letter);
+                $onceTimeFormatString .= $letter;
+            }
+            if (in_array($letter, static::$phpYearFormat)) {
+                $typeFormatLetters = 'YEAR';
+                $yearPosition[]    = strpos($format, $letter);
+                $yearFormatString  .= $letter;
+            }
+            if (in_array($letter, static::$phpMonthFormat)) {
+                $typeFormatLetters = 'MONTH';
+                $monthPosition[]   = strpos($format, $letter);
+                $monthFormatString .= $letter;
+            }
+            if (in_array($letter, static::$phpDaysFormat)) {
+                $typeFormatLetters = 'DAY';
+                $dayPosition[]     = strpos($format, $letter);
+                $dayFormatString   .= $letter;
+            }
+            if (!in_array($letter, $allFormat)) {
+                if ($typeFormatLetters == 'ONCE') {
+                    $onceTimeFormatString .= $letter;
+                } elseif ($typeFormatLetters == 'YEAR') {
+                    $yearFormatString .= $letter;
+                } elseif ($typeFormatLetters == 'MONTH') {
+                    $monthFormatString .= $letter;
+                } elseif ($typeFormatLetters == 'DAY') {
+                    $dayFormatString .= $letter;
+                } else {
+                    $unknownFormatString .= $letter;
+                }
+            }
+        }
+        if (empty($dayPosition)) {
+            throw new \Exception('It makes no sense to use list formatting without specifying the format of the day');
+        }
+
+        $minPosition = [];
+
+        if (!empty($onceTimePosition)) {
+            $minPosition['ONCE'] = min($onceTimePosition);
+        }
+
+        if (!empty($yearPosition)) {
+            $minPosition['YEAR'] = min($yearPosition);
+        }
+
+        $earlyMonth = null;
+        if (!empty($monthPosition)) {
+            $minPosition['MONTH'] = min($monthPosition);
+        }
+        $minPosition['DAY'] = min($dayPosition);
+        asort($minPosition);
+
         $interval  = new \DateInterval('P1D');
         $dateRange = new \DatePeriod($begin, $interval ,$end);
-        $monthList = [];
+        $yearList  = [];
         foreach($dateRange as $date){
-            $month               = $date->format('n');
-            $monthList[$month][] = $date;
+            $month                      = $date->format('n');
+            $year                       = $date->format('Y');
+            $yearList[$year][$month][]  = $date;
         }
+        $month                      = $dateRange->current->format('n');
+        $year                       = $dateRange->current->format('Y');
+        $yearList[$year][$month][]  = $dateRange->current;
 
-        $month = $dateRange->current->format('n');
-        $monthList[$month][] = $dateRange->current;
+        $listItems = [];
 
-        // If string month in format
-        if (strpos($format, 'F') !== false || strpos($format, 'f') !== false || strpos($format, 'M') !== false) {
-            if (strpos($format, 'F') !== false) {
-                $monthFormat = 'F';
-            }
-            if (strpos($format, 'f') !== false) {
-                $monthFormat = 'f';
-            }
-            if (strpos($format, 'M') !== false) {
-                $monthFormat = 'M';
-            }
-            $nonMonthFormat = trim(str_replace($monthFormat, '', $format));
-            $items          = [];
-            foreach ($monthList as $month => $dates) {
-                foreach ($dates as $date) {
-                    $items[] = self::format($nonMonthFormat, $date);
+        foreach ($yearList as $year => $monthList) {
+            $listItems[$year]['monthListItems'] = [];
+            foreach ($monthList as $month => $dayList) {
+                $listItems[$year]['month'][$month]['dayListItems'] = [];
+                foreach ($dayList as $date) {
+                    $listItems[$year]['month'][$month]['dayListItems'][] = trim(self::format($dayFormatString, $date, $caseDay));
                 }
-                $lastItem  = array_pop($items);
-                $items[]   = $lastItem . ' ' . self::format($monthFormat, $date);
+                $listItems[$year]['month'][$month]['dayListItemsString'] = implode(static::$listDelimiter, $listItems[$year]['month'][$month]['dayListItems']);
+                $listItems[$year]['month'][$month]['monthListItems']     = self::format($monthFormatString, $date, $caseMonth);
             }
-            $str = implode(', ', $items);
-        // Without month
-        } else {
-            $dates = [];
-            foreach ($monthList as $month) {
-                foreach ($month as $date) {
-                    $dates[] = self::format($format, $date);
-                }
-            }
-            $str = implode(', ', $dates);
+            $listItems[$year]['yearItem'] = $date->format($yearFormatString);
         }
-        return $str;
+        $onceTimeItem = $date->format($onceTimeFormatString);
+
+        $strArray = [];
+        if (strlen($unknownFormatString)) {
+            $strArray[] = $unknownFormatString;
+        }
+        foreach ($minPosition as $key => $value) {
+            if ($key == 'DAY') {
+                $lastMinValue = $value;
+                $yearStr      = [];
+                foreach ($listItems as $year => $yearEl) {
+                    $yearStrArray = [];
+                    foreach ($minPosition as $key => $value) {
+                        if ($value < $lastMinValue) {
+                            continue;
+                        }
+                        if ($key == 'ONCE') {
+                            $yearStrArray[] = $onceTimeItem;
+                        } elseif ($key == 'DAY') {
+                            $monthStrArray = [];
+                            foreach ($yearEl['month'] as $month => $monthEl) {
+                                $monthStrArray[] = trim($monthEl['dayListItemsString'] . ' ' . $monthEl['monthListItems']);
+                            }
+                            $yearStrArray[] = implode(static::$listDelimiter, $monthStrArray) . ' ';
+                        } elseif ($key == 'YEAR') {
+                            $yearStrArray[] = $yearEl['yearItem'];
+                        }
+                    }
+                    $yearStr[] = trim(implode('', $yearStrArray));
+                }
+                $strArray[] = implode(static::$listDelimiter, $yearStr);
+                break;
+            } elseif ($key == 'MONTH') {
+                $lastMinValue = $value;
+                $yearStr      = [];
+                foreach ($listItems as $year => $yearEl) {
+                    $yearStrArray = [];
+                    foreach ($minPosition as $key => $value) {
+                        if ($value <= $lastMinValue) {
+                            continue;
+                        }
+                        if ($key == 'ONCE') {
+                            $yearStrArray[] = $onceTimeItem;
+                        } elseif ($key == 'DAY') {
+                            $monthStrArray = [];
+                            foreach ($yearEl['month'] as $month => $monthEl) {
+                                $monthStrArray[] = trim($monthEl['monthListItems'] . $monthEl['dayListItemsString']);
+                            }
+                            $yearStrArray[] = implode(static::$listDelimiter, $monthStrArray) . ' ';
+                        } elseif ($key == 'YEAR') {
+                            $yearStrArray[] = $yearEl['yearItem'];
+                        }
+                    }
+                    $yearStr[] = trim(implode('', $yearStrArray));
+                }
+                $strArray[] = implode(static::$listDelimiter, $yearStr);
+                break;
+            } elseif ($key == 'YEAR') {
+                $lastMinValue = $value;
+                $yearStr      = [];
+                foreach ($listItems as $year => $yearEl) {
+                    $yearStrArray   = [];
+                    $yearStrArray[] = $yearEl['yearItem'];
+                    foreach ($minPosition as $key => $value) {
+                        if ($value <= $lastMinValue) {
+                            continue;
+                        }
+                        if ($key == 'DAY') {
+                            $monthStrArray = [];
+                            foreach ($yearEl['month'] as $month => $monthEl) {
+                                $monthStrArray[] = $monthEl['dayListItemsString'] . ' ' . $monthEl['monthListItems'];
+                            }
+                            $yearStrArray[] = implode(static::$listDelimiter, $monthStrArray);
+                            break;
+                        } elseif ($key == 'MONTH') {
+                            $monthStrArray = [];
+                            foreach ($yearEl['month'] as $month => $monthEl) {
+                                $monthStrArray[] = $monthEl['monthListItems'] . $monthEl['dayListItemsString'];
+                            }
+                            $yearStrArray[] = implode(static::$listDelimiter, $monthStrArray);
+                            break;
+                        }
+                    }
+                    $yearStr[] = trim(implode('', $yearStrArray));
+                }
+                $strArray[] = implode(static::$listDelimiter, $yearStr);
+                if (isset($minPosition['ONCE']) && $minPosition['ONCE'] > $value) {
+                    $strArray[] = ' ' . $onceTimeItem;
+                }
+                break;
+            } elseif ($key == 'ONCE') {
+                $strArray[] = $onceTimeItem;
+            }
+        }
+        return trim(implode('', $strArray));
     }
 
     public static function makeLongDay(int $day, int $type = 0): string {
---
+++ tests/ListFormatTest.php
@@ -0,0 +1,78 @@
+<?php
+
+use Mpakfm\RussianDateTime;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class FormatTest
+ * @covers RussianDateTime::listFormat
+ */
+class ListFormatTest extends TestCase {
+
+    public function testFormatOneMonthPeriod() {
+        $begin = date_create_from_format('d.m.Y H:i:s', '05.08.2019 12:00:00');
+        $end   = date_create_from_format('d.m.Y H:i:s', '06.08.2019 12:00:00');
+        $response = RussianDateTime::listFormat("в H:i l j Yг.", $begin, $end);
+        $this->assertEquals('в 12:00 Понедельник 5, Вторник 6 2019г.', $response);
+        $response = RussianDateTime::listFormat("j F", $begin, $end, 0, 1, '; ');
+        $this->assertEquals('5; 6 Августа', $response);
+        $response = RussianDateTime::listFormat("F j", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('Август 5, 6', $response);
+        $response = RussianDateTime::listFormat("F j Yг. в H:i", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('Август 5, 6 2019г. в 12:00', $response);
+        $response = RussianDateTime::listFormat("F j-ого H:i", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('Август 5-ого, 6-ого 12:00', $response);
+        $response = RussianDateTime::listFormat("j F в H:i", $begin, $end);
+        $this->assertEquals('5, 6 Августа в 12:00', $response);
+        $response = RussianDateTime::listFormat("Y F j", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('2019 Август 5, 6', $response);
+        $response = RussianDateTime::listFormat("H:i Y F j", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('12:00 2019 Август 5, 6', $response);
+        $response = RussianDateTime::listFormat("Y F j H:i", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('2019 Август 5, 6 12:00', $response);
+        $response = RussianDateTime::listFormat("Y F j, W неделя H:i", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('2019 Август 5, 32 неделя, 6, 32 неделя 12:00', $response);
+    }
+
+    public function testFormatTwoMonthPeriod() {
+        $begin = date_create_from_format('d.m.Y H:i:s', '30.08.2019 12:00:00');
+        $end   = date_create_from_format('d.m.Y H:i:s', '01.09.2019 12:00:00');
+        $response = RussianDateTime::listFormat("l j F", $begin, $end);
+        $this->assertEquals('Пятница 30, Суббота 31 Августа, Воскресение 1 Сентября', $response);
+        $response = RussianDateTime::listFormat("l j", $begin, $end);
+        $this->assertEquals('Пятница 30, Суббота 31, Воскресение 1', $response);
+        $response = RussianDateTime::listFormat("j F", $begin, $end);
+        $this->assertEquals('30, 31 Августа, 1 Сентября', $response);
+        $response = RussianDateTime::listFormat("j F Yг. H:i", $begin, $end);
+        $this->assertEquals('30, 31 Августа, 1 Сентября 2019г. 12:00', $response);
+        $response = RussianDateTime::listFormat("j F H:i", $begin, $end);
+        $this->assertEquals('30, 31 Августа, 1 Сентября 12:00', $response);
+        $response = RussianDateTime::listFormat("F j H:i", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('Август 30, 31, Сентябрь 1 12:00', $response);
+        $response = RussianDateTime::listFormat("Y F j", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('2019 Август 30, 31, Сентябрь 1', $response);
+        $response = RussianDateTime::listFormat("H:i Y F j", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('12:00 2019 Август 30, 31, Сентябрь 1', $response);
+        $response = RussianDateTime::listFormat("Y F j H:i", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('2019 Август 30, 31, Сентябрь 1 12:00', $response);
+    }
+
+    public function testFormatTwoYearPeriod() {
+        $begin = date_create_from_format('d.m.Y H:i:s', '30.12.2019 12:00:00');
+        $end   = date_create_from_format('d.m.Y H:i:s', '02.01.2020 12:00:00');
+        $response = RussianDateTime::listFormat("j F Yг.", $begin, $end);
+        $this->assertEquals('30, 31 Декабря 2019г., 1, 2 Января 2020г.', $response);
+        $response = RussianDateTime::listFormat("l j F", $begin, $end);
+        $this->assertEquals('Понедельник 30, Вторник 31 Декабря, Среда 1, Четверг 2 Января', $response);
+        $response = RussianDateTime::listFormat("l j F Yг. H:i", $begin, $end);
+        $this->assertEquals('Понедельник 30, Вторник 31 Декабря 2019г. 12:00, Среда 1, Четверг 2 Января 2020г. 12:00', $response);
+        $response = RussianDateTime::listFormat("F j Yг.", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('Декабрь 30, 31 2019г., Январь 1, 2 2020г.', $response);
+        $response = RussianDateTime::listFormat("Y F j", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('2019 Декабрь 30, 31, 2020 Январь 1, 2', $response);
+        $response = RussianDateTime::listFormat("H:i Y F j", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('12:00 2019 Декабрь 30, 31, 2020 Январь 1, 2', $response);
+        $response = RussianDateTime::listFormat("Y F j H:i", $begin, $end, RussianDateTime::FORMAT_NAME, RussianDateTime::FORMAT_NAME);
+        $this->assertEquals('2019 Декабрь 30, 31, 2020 Январь 1, 2 12:00', $response);
+    }
+}
